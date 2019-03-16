f0413f8e55290b261456d60258b437de6374c3f1
Heiner Alejandro <heiner.enis@gmail.com>
2019-03-16 09:45:46 -0400

    agregado static root

--- tracking/settings.py
+++ tracking/settings.py
@@ -167,6 +167,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 #DRFSO2_PROPRIETARY_BACKEND_NAME = 'google-oauth2'
